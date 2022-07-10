5f84aea9d16c2d6f2fe6d319c7e76909e62cef20
Shahbaj Alam <shahbajalam78@gmail.com>
2022-07-10 16:14:41 +0530

    Updated script

--- index.js
+++ index.js
@@ -2,38 +2,118 @@ const quoteText = document.querySelector("h1");
 const authorText = document.querySelector(".author");
 const tweetBtn = document.querySelector(".twitter__btn");
 const newQuoteBtn = document.querySelector(".new__quote--btn");
-const backdrop = document.querySelector(".backdrop");
-let quotes = [];
-let tweetUrl = "";
 
-async function getQuote() {
-    const apiUrl = "https://type.fit/api/quotes";
-    try {
-        const response = await fetch(apiUrl);
-        quotes = await response.json();
-    } catch (error) {
-        document.querySelector(".backdrop").classList.remove("hidden");
-        const errorDiv = document.querySelector(".error");
-        errorDiv.style.top = "5%";
-        errorDiv.style.opacity = "1";
-        errorDiv.innerHTML = `
+const backdrop = document.querySelector(".backdrop");
+const errorDiv = document.querySelector(".error");
+const count__exceeded = document.querySelector(".count__exceeded");
+
+let api_one = "https://api.quotable.io/random";
+let api__two = "https://quotable.io/quotes?page=";
+let api__three = "https://type.fit/api/quotes";
+let quotes = [];
+const apiUrlArray = [api_one, api__two, api__three];
+let tweetUrl = "";
+let quote, author;
+
+const showBackdrop = () => {
+    backdrop.classList.remove("hidden");
+};
+
+const hideBackdrop = () => {
+    backdrop.classList.add("hidden");
+};
+
+const showErrorModal = (error) => {
+    errorDiv.style.top = "5%";
+    errorDiv.style.opacity = "1";
+    errorDiv.innerHTML = `
         <h3>Something went wrong!! Unable to fetch quotes</h3> \n<h3 style="margin-top: 1rem">${error}</h3>
         `;
-        backdrop.addEventListener("click", () => {
-            backdrop.classList.add("hidden");
-            errorDiv.style.top = "-100%";
-            errorDiv.style.opacity = "0";
-        });
+};
+
+const hideErrorModal = () => {
+    errorDiv.style.top = "-100%";
+    errorDiv.style.opacity = "0";
+};
+
+const showTweetCountModal = () => {
+    count__exceeded.style.top = "5%";
+    count__exceeded.style.opacity = "1";
+    count__exceeded.innerHTML = `
+        <h3 style="margin-bottom:2rem;">Tweet contains more than 280 characters, some characters might get discarded</h3>\n
+        <button class="continue" style="font-size:1rem">Continue anyway</button>
+        `;
+    document.querySelector(".continue").addEventListener("click", () => {
+        hideBackdrop();
+        count__exceeded.style.top = "-100%";
+        count__exceeded.style.opacity = "0";
+        window.open(tweetUrl, "_blank");
+    });
+};
+
+async function getQuoteFromApiOne() {
+    let fetchedQuote;
+    let fetchedAuthor;
+    try {
+        const response = await fetch(api_one);
+        const data = await response.json();
+        fetchedQuote = data.content;
+        fetchedAuthor = data.author;
+    } catch (e) {
+        catchError(e);
     }
+    return [fetchedQuote, fetchedAuthor];
 }
 
-const displayQuote = () => {
-    let rand = Math.floor(Math.random() * quotes.length);
-    let randomQuote = quotes[rand].text;
-    let authorName = quotes[rand].author;
-    tweetUrl = `https://twitter.com/intent/tweet?text="${randomQuote}" - ${authorName}`;
+async function getQuoteFromApiTwo() {
+    let fetchedQuote;
+    let fetchedAuthor;
+    let pageNumber = Math.floor(Math.random() * 103) + 1;
+    try {
+        const response = await fetch(api__two + pageNumber);
+        const data = await response.json();
+        fetchedQuote = data.results[Math.floor(Math.random() * 20)].content;
+        fetchedAuthor = data.results[Math.floor(Math.random() * 20)].author;
+    } catch (e) {
+        catchError(e);
+    }
+    return [fetchedQuote, fetchedAuthor];
+}
 
-    if (randomQuote.length > 80) {
+async function getQuoteFromApiThree() {
+    let fetchedQuote;
+    let fetchedAuthor;
+    try {
+        const response = await fetch(api__three);
+        const data = await response.json();
+        let rand = Math.floor(Math.random() * data.length);
+        fetchedQuote = data[rand].text;
+        fetchedAuthor = data[rand].author;
+    } catch (e) {
+        catchError(e);
+    }
+    return [fetchedQuote, fetchedAuthor];
+}
+
+async function displayQuote() {
+    let returnedArray = [];
+    const randomSelectedApi = Math.floor(Math.random() * 3);
+    if (randomSelectedApi === 0) {
+        returnedArray = await getQuoteFromApiOne();
+    } else if (randomSelectedApi === 1) {
+        returnedArray = await getQuoteFromApiTwo();
+    } else {
+        returnedArray = await getQuoteFromApiThree();
+    }
+    quote = returnedArray[0];
+    author = returnedArray[1];
+    if (!quote) {
+        displayQuote();
+    }
+    if (!author) {
+        author = "Unknown";
+    }
+    if (quote.length > 80) {
         quoteText.style.fontSize = "1.5rem";
         const mediaQuery = window.matchMedia("(min-width: 40rem)");
         if (mediaQuery.matches) {
@@ -46,23 +126,42 @@ const displayQuote = () => {
             quoteText.style.fontSize = "2.5rem";
         }
     }
-    quoteText.innerHTML = randomQuote;
+    quoteText.innerHTML = quote;
+    authorText.innerHTML = `<em>${author}</em>`;
+}
 
-    if (!authorName) {
-        authorName = "Unknown";
-    }
-    authorText.innerHTML = `<em>${authorName}</em>`;
+const catchError = (error) => {
+    showBackdrop();
+    showErrorModal(error);
 };
 
 const tweetQuote = () => {
+    tweetUrl = `https://twitter.com/intent/tweet?text="${quote}" - ${author}`;
     if (!tweetUrl) {
+        return;
+    } else if (quote.length + author.length + 5 > 280) {
+        showBackdrop();
+        showTweetCountModal();
         return;
     } else {
         window.open(tweetUrl, "_blank");
     }
 };
 
-getQuote();
+const continueBtnHandler = () => {
+    window.open(tweetUrl, "_blank");
+    count__exceeded.style.top = "-100%";
+    count__exceeded.style.opacity = "0";
+    hideBackdrop();
+};
+
+const backdropHandler = () => {
+    count__exceeded.style.top = "-100%";
+    count__exceeded.style.opacity = "0";
+    hideErrorModal();
+    hideBackdrop();
+};
 
 newQuoteBtn.addEventListener("click", displayQuote);
 tweetBtn.addEventListener("click", tweetQuote);
+backdrop.addEventListener("click", backdropHandler);
